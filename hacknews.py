f26fc5068399a6f7f650f03d7a9a6c2e07aaa1ad
David <david.mitchell@digital-shokunin.net>
2013-12-16 15:16:03 -0500

    qt version

--- hacknews.py
+++ hacknews.py
@@ -1,85 +1,34 @@
-#!/usr/bin/env python3
 __author__ = 'David Mitchell'
+__version__ = '0.0.1
 
-from gi.repository import Gtk
-from gi.repository import AppIndicator3 as appindicator
-from hn import HN
+
 import sys
-from apscheduler.scheduler import Scheduler
+from PyQt4 import QtGui
+from hn import HN
 import webbrowser
 
 basedir = sys.path[0]
 
-hn = HN()
 wbhandle = webbrowser.get()
 
-#c = Config()
-
-#set defaults
-#config.setConfig(10, 5)
 num_stories = 10
 refresh_interval = 5
 
-#Setup refresh schedule
-sched = Scheduler()
-
-@sched.interval_schedule(minutes=refresh_interval)
-def refresh_list():
-    populate_menu()
-
-#sched.configure(options_from_ini_file)
-sched.start()
-
-def load_settings():
-    pass
-
-
-def menuitem_response(w, story):
+def menuitem_response(story):
     wbhandle.open_new(story["link"])
-    #print(story["link"])
-
 
 def populate_menu(m):
     # create and populate menu
     hn = HN()
     for story in hn.get_stories()[:num_stories]:
         story_item = bytes.decode(story["title"])
+        m.addAction(story_item)
 
-        #point_item = Gtk.button(Label=story["points"])
-
-        menu_item = Gtk.MenuItem(story_item)
-
-        m.append(menu_item)
-
-        # this is where you would connect your menu item up with a function:
-
-        menu_item.connect("activate", menuitem_response, story)
-
-        # show the items
-        menu_item.show()
-
-    sep_item = Gtk.SeparatorMenuItem()
-    sep_item.show()
-    m.append(sep_item)
-    quit_item = Gtk.MenuItem("Quit")
-    quit_item.connect("activate", Gtk.main_quit)
-    quit_item.show()
-    m.append(quit_item)
-
-    indie.set_menu(m)
-
-if __name__ == "__main__":
-    load_settings()
-    menu = Gtk.Menu()
-    indie = appindicator.Indicator.new(
-        "hacker-news-applet",
-        basedir + "/art/hack-news-icon.svg",
-        appindicator.IndicatorCategory.APPLICATION_STATUS)
-    indie.set_status(appindicator.IndicatorStatus.ACTIVE)
-    indie.set_attention_icon("connect_creating")
-
-    # create a menu
-    populate_menu(menu)
-
-
-    Gtk.main()
+app = QtGui.QApplication([])
+icon = QtGui.QSystemTrayIcon(QtGui.QIcon(basedir + "/art/hack-news-icon.svg"), app)
+menu = QtGui.QMenu()
+populate_menu(menu)
+menu.addAction("Quit", QtGui.qApp.quit)
+icon.setContextMenu(menu)
+icon.show()
+app.exec_()
