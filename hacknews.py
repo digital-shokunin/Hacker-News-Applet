ae1b5da4eb0bd324e7025a127c7d20ab7a445e27
David Mitchell <david.mitchell@digital-shokunin.net>
2013-12-27 02:27:08 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	hacknews.py
    
    Classified

--- hacknews.py
+++ hacknews.py
@@ -5,13 +5,13 @@ from gi.repository import Gtk
 from gi.repository import AppIndicator3 as appindicator
 from hn import HN
 import sys
-from apscheduler.scheduler import Scheduler
+#from apscheduler.scheduler import Scheduler
 import webbrowser
 
 basedir = sys.path[0]
 
-hn = HN()
-wbhandle = webbrowser.get()
+
+
 
 #c = Config()
 
@@ -20,66 +20,88 @@ wbhandle = webbrowser.get()
 num_stories = 10
 refresh_interval = 5
 
-#Setup refresh schedule
-sched = Scheduler()
 
-@sched.interval_schedule(minutes=refresh_interval)
-def refresh_list():
-    populate_menu()
+class HackerNewsIndicator:
+    def __init__(self):
+        wbhandle = webbrowser.get()
+        self.load_settings()
+        self.menu = Gtk.Menu()
+        self.indie = appindicator.Indicator.new(
+            "hacker-news-applet",
+            basedir + "/art/hack-news-icon.svg",
+            appindicator.IndicatorCategory.APPLICATION_STATUS)
+        self.indie.set_status(appindicator.IndicatorStatus.ACTIVE)
+        self.indie.set_attention_icon("connect_creating")
+
+        # create a menu
+        self.populate_menu(self.menu)
+
+
+        #GLib.timeout_add(300000, self.handler_timeout)
+#Setup refresh schedule
+#sched = Scheduler()
+
+#@sched.interval_schedule(minutes=refresh_interval)
+    def handler_timeout(self):
+        self.populate_menu(menu)
 
 #sched.configure(options_from_ini_file)
-sched.start()
+#sched.start()
 
-def load_settings():
-    pass
+    def load_settings(self):
+        pass
 
+    def menuitem_response(self, story):
+        self.wbhandle.open_new(story["link"])
+        #print(story["link"])
 
-def menuitem_response(w, story):
-    wbhandle.open_new(story["link"])
-    #print(story["link"])
+    def populate_menu(self, m):
+        # create and populate menu
+        hn = None
+        hn = HN()
+        for story in hn.get_stories()[:num_stories]:
+            story_item = bytes.decode(story["title"])
 
+            #point_item = Gtk.button(Label=story["points"])
 
-def populate_menu(m):
-    # create and populate menu
-    hn = HN()
-    for story in hn.get_stories()[:num_stories]:
-        story_item = bytes.decode(story["title"])
+            menu_item = Gtk.MenuItem(story_item)
 
-        #point_item = Gtk.button(Label=story["points"])
+            m.append(menu_item)
 
-        menu_item = Gtk.MenuItem(story_item)
+            # this is where you would connect your menu item up with a function:
 
-        m.append(menu_item)
+            menu_item.connect("activate", self.menuitem_response, story)
 
-        # this is where you would connect your menu item up with a function:
+            # show the items
+            menu_item.show()
 
-        menu_item.connect("activate", menuitem_response, story)
+        sep_item = Gtk.SeparatorMenuItem()
+        sep_item.show()
+        m.append(sep_item)
+        quit_item = Gtk.MenuItem("Quit")
+        quit_item.connect("activate", Gtk.main_quit)
+        quit_item.show()
+        m.append(quit_item)
 
-        # show the items
-        menu_item.show()
+        self.indie.set_menu(m)
 
-    sep_item = Gtk.SeparatorMenuItem()
-    sep_item.show()
-    m.append(sep_item)
-    quit_item = Gtk.MenuItem("Quit")
-    quit_item.connect("activate", Gtk.main_quit)
-    quit_item.show()
-    m.append(quit_item)
-
-    indie.set_menu(m)
+    def main(self):
+        Gtk.main()
 
 if __name__ == "__main__":
-    load_settings()
-    menu = Gtk.Menu()
-    indie = appindicator.Indicator.new(
-        "hacker-news-applet",
-        basedir + "/art/hack-news-icon.svg",
-        appindicator.IndicatorCategory.APPLICATION_STATUS)
-    indie.set_status(appindicator.IndicatorStatus.ACTIVE)
-    indie.set_attention_icon("connect_creating")
+    indie = HackerNewsIndicator()
+    indie.main()
+    #load_settings()
+    #menu = Gtk.Menu()
+    #indie = appindicator.Indicator.new(
+    #    "hacker-news-applet",
+    #    basedir + "/art/hack-news-icon.svg",
+    #    appindicator.IndicatorCategory.APPLICATION_STATUS)
+    #indie.set_status(appindicator.IndicatorStatus.ACTIVE)
+    #indie.set_attention_icon("connect_creating")
 
     # create a menu
-    populate_menu(menu)
+    #populate_menu(menu)
 
 
-    Gtk.main()
+    #Gtk.main()
