7ec7bf4988d8eec07e5e955d013c0add10735151
surge <david.mitchell@digital-shokunin.net>
2014-10-08 00:28:00 -0400

    Python2 version

--- hacknews.py
+++ hacknews.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 __author__ = 'David Mitchell'
 
-from gi.repository import Gtk
-from gi.repository import GLib
-from gi.repository import AppIndicator3 as appindicator
+import glib
+import gtk
+import appindicator
 from hn import HN
 import sys
 import webbrowser
@@ -25,22 +25,21 @@ class HackerNewsIndicator:
     def __init__(self):
         self.wbhandle = webbrowser.get()
         self.load_settings()
-        self.menu = Gtk.Menu()
-        self.indie = appindicator.Indicator.new(
-            "hacker-news-applet",
-            basedir + "/art/hack-news-icon.svg",
-            appindicator.IndicatorCategory.APPLICATION_STATUS)
-        self.indie.set_status(appindicator.IndicatorStatus.ACTIVE)
+        self.menu = gtk.Menu()
+        self.indie = appindicator.Indicator("hacker-news-applet", basedir + "/art/hack-news-icon.svg",
+                                 appindicator.CATEGORY_APPLICATION_STATUS)
+        self.indie.set_status(appindicator.STATUS_ACTIVE)
         self.indie.set_attention_icon("connect_creating")
 
         # create a menu
         self.populate_menu(self.menu)
 
         #Set refresh
-        GLib.timeout_add(300000, self.handler_timeout)
+        glib.timeout_add(300000, self.handler_timeout)
 
     def handler_timeout(self):
-        self.menu = Gtk.Menu()
+
+        self.menu = gtk.Menu()
         self.populate_menu(self.menu)
         return True
 
@@ -60,7 +59,7 @@ class HackerNewsIndicator:
 
             #point_item = Gtk.button(Label=story["points"])
 
-            menu_item = Gtk.MenuItem(story_item)
+            menu_item = gtk.MenuItem(story_item)
 
             m.append(menu_item)
 
@@ -71,18 +70,18 @@ class HackerNewsIndicator:
             # show the items
             menu_item.show()
 
-        sep_item = Gtk.SeparatorMenuItem()
+        sep_item = gtk.SeparatorMenuItem()
         sep_item.show()
         m.append(sep_item)
-        quit_item = Gtk.MenuItem("Quit")
-        quit_item.connect("activate", Gtk.main_quit)
+        quit_item = gtk.MenuItem("Quit")
+        quit_item.connect("activate", gtk.main_quit)
         quit_item.show()
         m.append(quit_item)
 
         self.indie.set_menu(m)
 
     def main(self):
-        Gtk.main()
+        gtk.main()
 
 if __name__ == "__main__":
     indie = HackerNewsIndicator()
